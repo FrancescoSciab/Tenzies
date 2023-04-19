8942559d098ebd990ceed2878cd73aa8b5c1ac07
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-04-19 12:34:48 +0100

    css + algorithm

--- tenzies-app/src/pages/_app.js
+++ tenzies-app/src/pages/_app.js
@@ -1,5 +1,27 @@
 import '@/styles/globals.css'
+import Die from './components/Die'
+import { useState } from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App() {
+
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+
+  const diceElements = dice.map(die => <Die value={die} />)  
+
+  return (
+    <main>
+      <div className='dice-container'>
+        {diceElements}
+      </div>
+    </main>
+  )
 }
